3091f5713a8a9dfaae66095162532d7eb9bba564
Guarrakesh <dario.guarracino2@gmail.com>
2021-01-15 00:04:30 +0100

    add owasp

--- appelli_microservice/spring/src/main/java/appelli/fixtures/PreloadAppelli.java
+++ appelli_microservice/spring/src/main/java/appelli/fixtures/PreloadAppelli.java
@@ -5,18 +5,26 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import appelli.entity.Appello;
 import appelli.repository.AppelliRepository;
+import org.springframework.core.env.Environment;
 
 import java.io.File;
+import java.util.Map;
 
 @Configuration
-public class PreloadAppelli {
+public class PreloadAppelli implements EnvironmentAware {
 
     private static final Logger log = LoggerFactory.getLogger(PreloadAppelli.class);
+    private Environment environment;
 
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
 
     @Bean
     CommandLineRunner initDatabase(AppelliRepository repo) {
@@ -25,7 +33,10 @@ public class PreloadAppelli {
             ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
             mapper.findAndRegisterModules();
 
-            Appello[] appelli = mapper.readValue(new File("/app/data/fixtures/appelli.yml"), Appello[].class);
+
+            Map<String, String> env = System.getenv();
+            String dataPath = env.get("DATA_PATH");
+            Appello[] appelli = mapper.readValue(new File(dataPath + "/fixtures/appelli.yml"), Appello[].class);
 
             for (Appello appello: appelli) {
                 log.info(appello.getAttDidId() + " : " + appello.getAttDidName());
